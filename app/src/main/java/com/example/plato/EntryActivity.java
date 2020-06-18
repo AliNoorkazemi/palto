23850f1df9640d0c7d5e3353b19f301b78bbcaf7
AliNoorkazemi <alinoorkazemi@gmail.com>
2020-06-19 00:49:07 +0430

    Entry Activity and Layout added.

--- app/src/main/java/com/example/plato/EntryActivity.java
+++ app/src/main/java/com/example/plato/EntryActivity.java
@@ -2,7 +2,10 @@ package com.example.plato;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EntryActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class EntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
+        Button logIn_button = findViewById(R.id.LogInButton);
+        Button register_button = findViewById(R.id.RegisterButton);
+
+        logIn_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_to_Login_page = new Intent(EntryActivity.this,LoginActivity.class);
+                startActivity(intent_to_Login_page);
+            }
+        });
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_to_Login_page = new Intent(EntryActivity.this,RegisterActivity.class);
+                startActivity(intent_to_Login_page);
+            }
+        });
     }
 }
