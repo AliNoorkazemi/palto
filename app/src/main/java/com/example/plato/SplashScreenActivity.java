046d7ca4322ea68bc66595064cef3efaa91bba14
AliNoorkazemi <alinoorkazemi@gmail.com>
2020-06-19 00:32:32 +0430

    manifest changed.

---
+++ app/src/main/java/com/example/plato/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.plato;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2500;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent_to_main = new Intent(SplashScreenActivity.this,EntryActivity.class);
+                startActivity(intent_to_main);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
