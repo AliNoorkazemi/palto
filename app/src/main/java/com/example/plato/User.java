6a1f8841571c4fe5bd808aa5812fbe7fabb689ce
AliNoorkazemi <alinoorkazemi@gmail.com>
2020-06-19 01:53:12 +0430

    User setter added.

--- app/src/main/java/com/example/plato/User.java
+++ app/src/main/java/com/example/plato/User.java
@@ -2,10 +2,10 @@ package com.example.plato;
 
 public class User {
 
-    private String password;
     private String user_name;
+    private String password;
 
-    public User(String password, String user_name) {
+    public User(String user_name, String password) {
         this.password = password;
         this.user_name = user_name;
     }
@@ -16,5 +16,13 @@ public class User {
 
     public String getUser_name() {
         return user_name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
     }
 }
