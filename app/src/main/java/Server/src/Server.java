631e423e0f0dbe6c4a9865e63049dcdf12abe0ae
AliNoorkazemi <alinoorkazemi@gmail.com>
2020-06-19 22:17:34 +0430

    Added server basic and users file.

--- app/src/main/java/Server/src/Server.java
+++ app/src/main/java/Server/src/Server.java
@@ -1,5 +1,33 @@
+import user.User;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
 
+    static Map<User,ClientHandler> users = new ConcurrentHashMap<>();
+    public static void main(String[] args) {
+        try {
+            FileInputStream file = new FileInputStream("Users.txt");
+            ObjectInputStream ois = new ObjectInputStream(file);
+            users = (ConcurrentHashMap)ois.readObject();
+            file.close();
+            ois.close();
+
+            ServerSocket serverSocket = new ServerSocket(6666);
+
+
+
+            while(true){
+                Socket socket = serverSocket.accept();
+                Thread clientHandler = new Thread(new ClientHandler(socket));
+                clientHandler.start();
+            }
+        }catch(IOException | ClassNotFoundException io){
+            io.printStackTrace();
+        }
+    }
 }
