5857aee9f9532306ea185ceea1302e93cc7e5472
ehsan <ehsan.moghadam1396@gmail.com>
2020-06-20 22:11:07 +0430

    Update profileActivity:add avatar image ,username and bio. also Add Intent in drawerlayout on profile item

--- app/src/main/java/com/example/plato/MainActivity.java
+++ app/src/main/java/com/example/plato/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.plato;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
@@ -33,25 +36,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_navigation);
 
 
-        toolbar=findViewById(R.id.toolbar_mainActivity);
-        toolbarTitle_tv=findViewById(R.id.tv_mainActivity_toolbarTitle);
+        toolbar = findViewById(R.id.toolbar_mainActivity);
+        toolbarTitle_tv = findViewById(R.id.tv_mainActivity_toolbarTitle);
         setSupportActionBar(toolbar);
 
 
-        drawerLayout=findViewById(R.id.drawerlayout_mainActivity);
-        drawerNavigationView=findViewById(R.id.navigationView_main_drawer);
-        ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer,R.string.closeDrawer);
+        drawerLayout = findViewById(R.id.drawerlayout_mainActivity);
+        drawerNavigationView = findViewById(R.id.navigationView_main_drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getColor(R.color.white));
         actionBarDrawerToggle.syncState();
 
+        drawerNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Log.i("MainLog", "onNavigationItemSelected: ");
+                switch (menuItem.getItemId()) {
+                    case R.id.btn_drawerNavigation_profile:
+                        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                        TextView textView = findViewById(R.id.tv_mainActivity_header_username);
+                        ImageView imageView = findViewById(R.id.iv_mainActivity_header_avatarimg);
+                        intent.putExtra("USERNAME", textView.getText().toString());
+                        intent.putExtra("PROFILE_IMAGE", imageView.getId());
+                        startActivity(intent);
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return false;
+            }
+        });
 
 
         setFragment(new HomeFrag());
-
 
 
         navigationView = findViewById(R.id.bottomNav_mainActivity);
@@ -85,10 +105,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean setFragment(Fragment fragment) {
-        if(fragment!=null){
+        if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.frameLayout_mainActivity_fragmentHolder,fragment)
+                    .replace(R.id.frameLayout_mainActivity_fragmentHolder, fragment)
                     .commit();
             return true;
         }
