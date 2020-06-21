907219623e4641fbd1416f78b5b97817d5a3c5a5
AliNoorkazemi <alinoorkazemi@gmail.com>
2020-06-21 17:47:35 +0430

    a

--- app/src/main/java/com/example/plato/entry/EntryActivity.java
+++ app/src/main/java/com/example/plato/entry/EntryActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.plato.MainActivity;
 import com.example.plato.R;
 
 public class EntryActivity extends AppCompatActivity {
@@ -18,6 +19,15 @@ public class EntryActivity extends AppCompatActivity {
 
         Button logIn_button = findViewById(R.id.LogInButton);
         Button register_button = findViewById(R.id.RegisterButton);
+        Button to_main_btn = findViewById(R.id.MainButton);
+
+        to_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent to_main_intent = new Intent (EntryActivity.this, MainActivity.class);
+                startActivity(to_main_intent);
+            }
+        });
 
 
         logIn_button.setOnClickListener(new View.OnClickListener() {
