10da73aa4bb10f64f8f72f42958d1752b7d1fe30
ehsan <ehsan.moghadam1396@gmail.com>
2020-06-22 17:52:00 +0430

    Create User class in android for interaction by service

---
+++ app/src/main/java/com/example/plato/User.java
@@ -0,0 +1,15 @@
+package com.example.plato;
+
+import com.example.plato.Fragment.Friend;
+
+public class User {
+    private Friend friend;
+
+    public Friend getFriend() {
+        return friend;
+    }
+
+    public void setFriend(Friend friend) {
+        this.friend = friend;
+    }
+}
